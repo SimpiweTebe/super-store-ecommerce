a62c539f0dc4d7c9f7ca495ee507c903b4b9177d
SimphiweTebe <simphiwetebe@gmail.com>
2025-11-19 22:06:28 +0300

    Completed font setup for android and IOS

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
+  SafeAreaView,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
@@ -17,28 +18,49 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.figLight}>
+            Special for you light font
+          </Text>
+          <Text style={styles.figItalic}>
+            Special for you italic font
+          </Text>
+          <Text style={styles.figBold}>
+            Special for you bold font
+          </Text>
+          <Text style={styles.figExtraBold}>
+            Special for you extra bold font
+          </Text>
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "red",
     flex: 1,
+    justifyContent: "center",
+    // alignItems: "center",
+  },
+  figLight: {
+    fontFamily: "Figtree-Light",
+    fontSize: 20,
+  },
+  figBold: {
+    fontFamily: "Figtree-Bold",
+    fontSize: 20,
+  },
+  figItalic: {
+    fontFamily: "Figtree-Light",
+    fontStyle: 'italic',
+    fontSize: 20,
+  },
+  figExtraBold: {
+    fontFamily: "Figtree-ExtraBold",
+    fontSize: 20,
   },
 });
 
